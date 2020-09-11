daf55eafb67dffa4b4c06de9bb78561224e49370
Dayvson Silva <ddss.snf19@uea.edu.br>
2020-09-11 10:42:07 -0400

    Refatorando o mainactivity

--- app/src/main/java/br/com/maypay/calculadora/MainActivity.kt
+++ app/src/main/java/br/com/maypay/calculadora/MainActivity.kt
@@ -1,12 +1,12 @@
 package br.com.maypay.calculadora
 
 import android.os.Bundle
-import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.screen.*
+import kotlinx.android.synthetic.main.simple_cal_buttons.*
 import org.mariuszgromada.math.mxparser.Expression
-import java.util.regex.Pattern
 
 
 class MainActivity : AppCompatActivity() {
@@ -16,36 +16,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.calculator_layout)
 
         /* Number Buttons */
-        var btnZero = findViewById<Button>(R.id.btnZero)
-        var btnOne = findViewById<Button>(R.id.btnOne)
-        var btnTwo = findViewById<Button>(R.id.btnTwo)
-        var btnThree = findViewById<Button>(R.id.btnThree)
-        var btnFour = findViewById<Button>(R.id.btnFour)
-        var btnFive = findViewById<Button>(R.id.btnFive)
-        var btnSix = findViewById<Button>(R.id.btnSix)
-        var btnSeven = findViewById<Button>(R.id.btnSeven)
-        var btnEight = findViewById<Button>(R.id.btnEight)
-        var btnNine = findViewById<Button>(R.id.btnNine)
-
-        var btnDecimal = findViewById<Button>(R.id.btnDecimal)
-        var btnDoubleZero = findViewById<Button>(R.id.btnDoubleZero)
-
-        /* Operator Buttons */
-        var btnClear = findViewById<Button>(R.id.btnClear);
-        var btnPercentage = findViewById<Button>(R.id.btnPercentage);
-        var btnDivide = findViewById<Button>(R.id.btnDivide);
-        var btnMultiply = findViewById<Button>(R.id.btnMultiply);
-        var btnMinus = findViewById<Button>(R.id.btnMinus);
-        var btnAdd = findViewById<Button>(R.id.btnAdd);
-        var btnEqual = findViewById<Button>(R.id.btnEqual);
-
-        var tvExpression = findViewById<TextView>(R.id.tvExpression);
-        var tvResult = findViewById<TextView>(R.id.tvResult);
-
-        btnClear.setOnClickListener { clearScreen(tvExpression, tvResult) }
-
-        btnEqual.setOnClickListener { showResult(tvExpression, tvResult) }
-
         btnOne.setOnClickListener { writeExpression("1", tvExpression) }
         btnTwo.setOnClickListener { writeExpression("2", tvExpression) }
         btnThree.setOnClickListener { writeExpression("3", tvExpression) }
@@ -56,22 +26,23 @@ class MainActivity : AppCompatActivity() {
         btnEight.setOnClickListener { writeExpression("8", tvExpression) }
         btnNine.setOnClickListener { writeExpression("9", tvExpression) }
         btnZero.setOnClickListener { writeExpression("0", tvExpression) }
+
         btnDoubleZero.setOnClickListener { writeExpression("00", tvExpression) }
         btnDecimal.setOnClickListener { writeExpression(".", tvExpression) }
 
+        /* Operator Buttons */
         btnPercentage.setOnClickListener { writeExpression("%", tvExpression) }
         btnDivide.setOnClickListener { writeExpression("/", tvExpression) }
         btnMultiply.setOnClickListener { writeExpression("*", tvExpression) }
         btnMinus.setOnClickListener { writeExpression("-", tvExpression) }
         btnAdd.setOnClickListener { writeExpression("+", tvExpression) }
 
+        btnClear.setOnClickListener { clearScreen(tvExpression, tvResult) }
+        btnEqual.setOnClickListener { showResult(tvExpression, tvResult) }
+
     }
 
-    fun percentage(value: String, tvExpression: TextView){
-        val b = Pattern.matches("a*b", "aaaaab")
-    }
-
-    fun writeExpression(value: String, tvExpression: TextView) {
+    private fun writeExpression(value: String, tvExpression: TextView) {
         var expression: String = tvExpression.text.toString()
         expression += value
         tvExpression.text = expression
